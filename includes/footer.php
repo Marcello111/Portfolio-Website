55e639d58ef230266605e1370334215359de06dd
Marcel <59026719+Marcello111@users.noreply.github.com>
2023-08-08 20:17:55 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,70 +1,72 @@
-<!-- Footer -->
-<footer class="footer">
-      <div class="footer__inner container">
-        <p class="heading-quaternary">Marcell&apos;s Website</p>
-        <div class="footer__information">
-          <div class="footer__text-box">
-            <p class="paragraph">
-              &#169; Marcell, <span class="footer__year">2021</span>
-            </p>
-            <p class="paragraph paragraph--attr">
-              Illustrations are from undraw.com
-            </p>
-            <p class="paragraph paragraph--attr">
-              Icon made by Freepik from www.flaticon.com
-            </p>
-            <p class="paragraph paragraph--attr">
-              Icon made by the makers of Tailwind CSS from www.heroicons.com
-            </p>
-          </div>
-          <div>
-            <div class="footer__link-box"></div>
-            <ul class="footer__socials-box">
-              <li class="icon icon--small">
-                <a href="#" aria-label="Email icon">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="#2E868C"
-                    class="w-6 h-6 icon__img"
-                  >
-                    <path
-                      d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z"
-                    />
-                    <path
-                      d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li class="icon icon--small">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <img
-                    src="img/social-media.svg"
-                    class="icon__img"
-                    alt="Linkedin icon"
-                  />
-                </a>
-              </li>
-              <li class="icon icon--small">
-                <a
-                  href="https://github.com/Marcello111/"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <img
-                    src="img/square.svg"
-                    class="icon__img"
-                    alt="Github icon"
-                  />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-</footer>
+            <!-- Footer -->
+            <footer class="footer">
+                  <div class="footer__inner container">
+                    <p class="heading-quaternary">Marcell&apos;s Website</p>
+                    <div class="footer__information">
+                      <div class="footer__text-box">
+                        <p class="paragraph">
+                          &#169; Marcell, <span class="footer__year">2023</span>
+                        </p>
+                        <p class="paragraph paragraph--attr">
+                          Illustrations are from undraw.com
+                        </p>
+                        <p class="paragraph paragraph--attr">
+                          Icon made by Freepik from www.flaticon.com
+                        </p>
+                        <p class="paragraph paragraph--attr">
+                          Icon made by the makers of Tailwind CSS from www.heroicons.com
+                        </p>
+                      </div>
+                      <div>
+                        <div class="footer__link-box"></div>
+                        <ul class="footer__socials-box">
+                          <li class="icon icon--small">
+                            <a href="#" aria-label="Email icon">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="#2E868C"
+                                class="w-6 h-6 icon__img"
+                              >
+                                <path
+                                  d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z"
+                                />
+                                <path
+                                  d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"
+                                />
+                              </svg>
+                            </a>
+                          </li>
+                          <li class="icon icon--small">
+                            <a
+                              href="#"
+                              target="_blank"
+                              rel="noopener"
+                            >
+                              <img
+                                src="img/social-media.svg"
+                                class="icon__img"
+                                alt="Linkedin icon"
+                              />
+                            </a>
+                          </li>
+                          <li class="icon icon--small">
+                            <a
+                              href="https://github.com/Marcello111/"
+                              target="_blank"
+                              rel="noopener"
+                            >
+                              <img
+                                src="img/square.svg"
+                                class="icon__img"
+                                alt="Github icon"
+                              />
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+            </footer>
+      </body>
+</html>
